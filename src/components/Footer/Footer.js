cfe51a552cc31abd71e53ae1ea92a24758ea9184
Kanyisa Ntombini <71196826+Kanyisa-Ntombini@users.noreply.github.com>
2024-03-05 03:27:36 +0200

    Initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <h2 className="Footer-Title">Footer</h2>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import './Main.css';
+
+function Main() {
+  return (
+    <main>
+      <h2 className="Main-Title">Main</h2>
+    </main>
+  );
+}
+
+export default Main;
